f1f1dced16a96d25810ef28f01e2ca4e5e649e71
prashantkanwar <prashant.kanwar2015@gmail.com>
2023-10-17 12:27:38 +0900

    Operators4 is  committed.

---
+++ src/main/kotlin/Operators4.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var a = 5
+    print(" add the value of a and 2 = ")
+    a += 2 // a = a + 2
+    println(a) // 7
+    print("subtract 4 from the value of a = ")
+    a -= 4 // a = a - 4
+    println(a) // 3
+    print("multiply by 5 to the value of a = ")
+    a *= 5
+    println(a) // 15
+    print("divide the value of a by 5")
+    a /= 5
+    println(a) // 3 (beware integer division)
+    print("a mode 2 = ")
+    a %= 2
+    println(a) // 1
+}
