6482baf0d63e384e922607ba091ba2ba0fff4dcb
prashantkanwar <prashant.kanwar2015@gmail.com>
2023-10-17 11:32:12 +0900

    Operators1 commited.

---
+++ src/main/kotlin/Operators1.kt
@@ -0,0 +1,33 @@
+fun main() {
+    val add = 4 + 2
+    print("sum of 4 and 2 = ")
+    println(add) // 6
+
+    val minus = 4 - 2
+    print("Subtraction of 4 and 2 = ")
+    println(minus) // 2
+
+    val mul = 4 * 2
+    print("multiplication of 4 and 2 =")
+    println(mul) // 8
+
+    val divide = 4 / 2
+    print("Divide 4 by 2 and the result = ")
+    println(divide) // 2
+
+    val divide2 = 7 / 2
+    print("Divide 7 by 2 and the result = ")
+    println(divide2) // 3 (!)
+
+    val divide3 = 8 / 2
+    print("Divide 8 by 2 and the result = ")
+    println(divide3) // 4
+
+    val divide4 = 7 / 2.0
+    print("Divide 7 by 2.0 and the result = ")
+    println(divide4) // 3.5 (!)
+
+    val remainder = 7 % 2
+    print("7 Mode 2 and the result = ")
+    println(remainder) // 1
+}
