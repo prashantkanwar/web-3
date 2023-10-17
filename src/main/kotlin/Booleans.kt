2db00d80972122bf7bdc5df2c6278d041d89dfc3
prashantkanwar <prashant.kanwar2015@gmail.com>
2023-10-18 06:49:25 +0900

    Booleans is  committed.

---
+++ src/main/kotlin/Booleans.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val a = true
+    val b: Boolean = false // explicit type is not necessary
+    print("The value of a = ")
+    println(a)
+    print("The value of b = ")
+    println(b)
+}
