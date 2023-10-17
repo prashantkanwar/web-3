27ba856aa1f0a57abad360b9660178755c2e1268
prashantkanwar <prashant.kanwar2015@gmail.com>
2023-10-17 12:20:40 +0900

    Imports is  committed.

---
+++ src/main/kotlin/Imports.kt
@@ -0,0 +1,12 @@
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.round
+
+fun main() {
+    print("Round of value of 7.6 = ")
+    println(round(7.6)) // 8.0
+    print("floor value of 7 = ")
+    println(floor(7.6)) // 7.0
+    print("ceil value of 7.1 = ")
+    println(ceil(7.1)) // 8.0
+}
